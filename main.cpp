a2ac0d2af9b9eb069ac53eafe0badd314eaa1eb6
ZEVS1206 <egor_bud5@inbox.ru>
2024-08-14 18:53:18 +0300

    Working version

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ struct Discriminant_and_imagine_units{
     int count_of_iu;
 };
 
-struct Discriminant_and_imagine_units *finding_discriminant(float coefficient_b, float coefficient_c, float coefficient_a, struct Discriminant_and_imagine_units *);/*discriminant*/
+void finding_discriminant(float coefficient_b, float coefficient_c, float coefficient_a, struct Discriminant_and_imagine_units *);/*discriminant*/
 void finding_solutions(float coefficient_a, float coefficient_b, struct Discriminant_and_imagine_units *);/*Finding solutions*/
 
 int main(){
@@ -23,24 +23,22 @@ int main(){
     printf("Введите коэффициент c:");
     scanf("%f", &coefficient_c);
     printf("\n");
-    struct Discriminant_and_imagine_units *element;// = (struct Discriminant_and_imagine_units *) malloc(sizeof(struct Discriminant_and_imagine_units));
-    element = finding_discriminant(coefficient_b, coefficient_c, coefficient_a, &element);/*discriminant*/
+    struct Discriminant_and_imagine_units element;// = (struct Discriminant_and_imagine_units *) malloc(sizeof(struct Discriminant_and_imagine_units));
+    finding_discriminant(coefficient_b, coefficient_c, coefficient_a, &element);/*discriminant*/
     finding_solutions(coefficient_a, coefficient_b, &element);
     return 0;
 }
 
-struct Discriminant_and_imagine_units *finding_discriminant(float coefficient_b, float coefficient_c, float coefficient_a, struct Discriminant_and_imagine_units *element){
+void finding_discriminant(float coefficient_b, float coefficient_c, float coefficient_a, struct Discriminant_and_imagine_units *element){
     float discr2 = coefficient_b * coefficient_b - 4 * coefficient_a * coefficient_c;/*D**2*/
     /*D[0] - discriminant, D[1] - count of imaginary units*/
     if (discr2 >= 0){/*There are real solutions*/
         element->discriminant = sqrt(discr2);
         element->count_of_iu = 0;
-        return element;
     } else {/*There are complex solutions*/
         discr2 = -discr2;
         element->discriminant = sqrt(discr2);
         element->count_of_iu = 1;
-        return element;
     }
 }
 
